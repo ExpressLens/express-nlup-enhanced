27f30e369b681581ef6caee509175074b49b3100
ExpressLens <expresslens@proton.me>
2023-08-20 14:56:11 +0000

    add cmd

--- nlup/confusion.py
+++ nlup/confusion.py
@@ -11,4 +11,26 @@
 # The above copyright notice and this permission notice shall be included
 # in all copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+"""Confusion matrix classes."""
+
+
+from math import sqrt
+from functools import partial
+from collections import defaultdict
+
+
+NAN = float("nan")
+INF = float("inf")
+
+
+class ConfusionMixin(object):
+  """Confusion methods shared by all three non-abstract classes."""

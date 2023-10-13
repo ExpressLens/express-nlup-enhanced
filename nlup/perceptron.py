7880cbb52acdf2ca5c3c86e8a10f1b5cfec242cd
ExpressLens <expresslens@proton.me>
2023-10-13 15:18:42 +0000

    Upgrade some dependencies

--- nlup/perceptron.py
+++ nlup/perceptron.py
@@ -11,4 +11,19 @@
 # The above copyright notice and this permission notice shall be included
 # in all copies or substantial portions of the Software.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF A
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# PLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+"""Perceptron-like classifers, including:
+
+* `BinaryPerceptron`: binary perceptron classifier
+* `Perceptron`: multiclass perceptron classifier
+* `SequencePerceptron`: multiclass perceptron for sequence tagging
+* `BinaryAveragedPerceptron`: binary averaged perceptron classifier
+* `AveragedPerceptron`: multiclass aver
